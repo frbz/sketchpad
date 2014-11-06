ea1bbd02e76957b674efc6012ba3f7b6e250de97
Rik Godschalk <samuraipastacat@gmail.com>
2014-11-06 14:54:24 +0100

    Some styling and added random colors button

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
 $(document).ready(function() {
-	generateGrid(20,20);
-
-	$(".row > div").mouseenter(function() {
-		$(this).addClass("highlight");
-	});
-
-	$(".button:nth-child(2)").click(function() {
-		$(".row > div").removeClass("highlight");
-	});
+	generateGrid(10, 1);
 
 	$(".button:first").click(function() {
 		$("#container").empty();
-		var rows = +prompt("How many rows would you like to generate?");
-		var cells = +prompt("And how many columns?");
-		// function doesn seem to work inside here
-		generateGrid(rows, cells);
+		var gridsize = +prompt("How many rows would you like to generate?");
+		generateGrid(gridsize, 1);
+	});
+
+	$(".button:nth-child(2)").click(function() {
+		$("#container").empty();
+		var gridsize = +prompt("How many rows would you like to generate?");
+		generateGrid(gridsize, 2);
+	});
+
+	$(".button:nth-child(3)").click(function() {
+		$("#container").empty();
+		var gridsize = +prompt("How many rows would you like to generate?");
+		generateGrid(gridsize, 3);
+	});
+
+	$(".button:nth-child(4)").click(function() {
+		$("#container > div > div").css("background-color", "white");
 	});
 });
 
-function generateGrid(rows, cells) {
-	var rh = 800 / rows;
-	var cw = 800 / cells;
-	for ( i = 0; i < rows; i++) {
+function generateGrid(size, color) {
+	var grid = 800 / size;
+	for ( i = 0; i < size; i++) {
 		// need to set row class properties to prevent bugs with scaling
-		$("#container").append("<div class='row'></div>")
+		$("#container").append("<div></div>")
 	}
-	for ( j = 0; j < cells; j++) {
-		$(".row").append("<div></div>")
+	for ( j = 0; j < size; j++) {
+		$("#container > div").append("<div></div>")
 	}
-	$(".row > div").css({"height": rh, "width": cw, "display": "inline-block", 
-		"margin-bottom": "-5px"})
+	$("#container > div > div").css({"height": grid, "width": grid, "display": "inline-block", 
+		"margin-bottom": "-5px"});
+	$("#container > div > div").mouseenter(function() {
+		if ( color === 1 ) {
+			$(this).css("background-color", "#EC583A");
+		}
+		else if ( color === 2) {
+  		var random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+			$(this).css("background-color", random);
+		}
+		else if (color === 3) {
+			$(this).css("opacity", "+=0.1")
+		}
+	});
 };
